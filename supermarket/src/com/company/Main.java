c9d392b60ac05edfe80cbccd0082fb454ebad70a
Yona Moreda <36438877+YonaMoreda@users.noreply.github.com>
2021-05-19 17:17:41 +0200

    Add files via upload

--- supermarket/src/com/company/Main.java
+++ supermarket/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import com.company.model.CashRegister;
+import com.company.model.ShoppingCart;
+import com.company.model.products.Soap;
+
 public class Main {
 
     public static void main(String[] args) {
+        CashRegister cashRegister = new CashRegister();
+
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.addProduct(new Soap());
+
+        cashRegister.checkout(shoppingCart, 10);
 
     }
 }
--- supermarket/src/com/company/model/CashRegister.java
+++ supermarket/src/com/company/model/CashRegister.java
@@ -4,6 +4,8 @@ import com.company.model.products.Product;
 
 public class CashRegister {
 
+    private static final String EUROS = " euros";
+
     public void checkout(ShoppingCart shoppingCart, double moneyPaid) {
         System.out.println("Now checking out.");
 
@@ -16,13 +18,14 @@ public class CashRegister {
         if (sumTotal > 10) {
             discountAmount = 0.5;
         }
-       if (sumTotal > 20) {
-           discountAmount = 1;
-       }
+        if (sumTotal > 20) {
+            discountAmount = 1;
+        }
 
         double finalAmount = (sumTotal - discountAmount);
-        System.out.println("Amount to be paid: " + sumTotal + " with discount: " + discountAmount + " => " +
-                finalAmount + "total");
-        System.out.println("The change: " + (finalAmount - moneyPaid));
+        System.out.println("Amount to be paid: " + sumTotal + EUROS);
+        System.out.println("Discount amount: " + discountAmount + EUROS);
+        System.out.println("Total amount: " + finalAmount + EUROS);
+        System.out.println("Change amount: " + (moneyPaid - finalAmount) + EUROS);
     }
 }
