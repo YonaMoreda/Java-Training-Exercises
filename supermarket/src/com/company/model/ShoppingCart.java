5be3feb9ed7a8de7e3094490bf6d58c46af0d8b6
Gillis J. de Nijs <gillis.de-nijs@capgemini.com>
2021-06-02 09:46:56 +0200

    Change ArrayList to List interface. Return unmodifiableList to prevent cart.getProducts().add() or any other list operations (encapsulation). Make products final, since it's only assigned once.

--- supermarket/src/com/company/model/ShoppingCart.java
+++ supermarket/src/com/company/model/ShoppingCart.java
@@ -3,17 +3,19 @@ package com.company.model;
 import com.company.model.products.Product;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ShoppingCart {
 
-    private ArrayList<Product> products;
+    private final List<Product> products;
 
     public ShoppingCart() {
         this.products = new ArrayList<>();
     }
 
-    public ArrayList<Product> getProducts() {
-        return products;
+    public List<Product> getProducts() {
+        return Collections.unmodifiableList(products);
     }
 
     public void addProduct(Product product) {
